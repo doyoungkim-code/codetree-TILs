372a2facb95041891d4d42b25fd1f94a17206d3d
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-07-08 22:04:53 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240708/장학금/scholarship.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if(a >= 90)
+    {
+        if(b >= 95)
+            printf("10");
+        else if(b >= 90)
+            printf("5");
+        else
+            printf("0");
+    }
+    else
+        printf("0");
+    return 0;
+}
