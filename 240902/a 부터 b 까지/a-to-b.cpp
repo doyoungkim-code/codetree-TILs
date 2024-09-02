3300024a79657f08283c8d51a387d6e6334aa2e0
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-09-02 15:36:45 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/a 부터 b 까지/a-to-b.cpp
+++ 240902/a 부터 b 까지/a-to-b.cpp
@@ -8,9 +8,9 @@ int main() {
     {
         cout << a << " ";
         if(a % 2 == 1)
-            a == a * 2;
+            a = a * 2;
         else
-            a == a + 3;
+            a = a + 3;
     }
     return 0;
 }
