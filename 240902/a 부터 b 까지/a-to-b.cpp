590b37b930aab5b6575b0e96dbfae1284da077fb
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-09-02 15:35:47 +0900

    Title: a 부터 b 까지, Time: 1096ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240902/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while(a <= b)
+    {
+        cout << a << " ";
+        if(a % 2 == 1)
+            a == a * 2;
+        else
+            a == a * 3;
+    }
+    return 0;
+}
