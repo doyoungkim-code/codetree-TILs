3fbe94febcc251f51b479e88bf9f5c458df5a811
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-09-05 00:56:28 +0900

    Title: 배수의 개수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240905/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a;
+    int c1;
+    int c2;
+
+    n = 10;
+    c1 = 0;
+    c2 = 0;
+    while(n)
+    {
+        cin >> a;
+        if(a % 3 == 0)
+            c1 ++;
+        else if (a % 5 == 0)
+            c2 ++;
+        n --;
+    }
+    cout << c1 << " " << c2;
+    return 0;
+}
