d549ce93f1a1b4d4cb398eec561a4d7d74787d7b
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-09-06 01:48:35 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int i = 0;
+    int gyo = 0;
+    int bok = 0;
+    int hwa = 0;
+
+    cin >> a;
+    while(i < a)
+    {
+        if (i == 0)
+        {
+            i ++;
+            continue ;
+        }
+        if(i % 12 == 0)
+            hwa ++;
+        else if(i % 3 == 0)
+            bok ++;
+        else if(i % 2 == 0)
+            gyo ++;
+        i ++;
+    }
+    cout << gyo << " " << bok << " " << hwa;
+    return 0;
+}
