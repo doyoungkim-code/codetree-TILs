ff32ecf8c0d4d440859b99ec58cc1f04954a13bf
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-09-22 23:46:07 +0900

    Title: 특정 조건의 온전수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240922/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i = 0;
+
+    cin >> n;
+    while(i++ <= n)
+    {
+        if (i % 2 == 0)
+            continue;
+        if (i % 10 == 5)
+            continue;
+        if (i % 3 == 0 && i % 9 != 0)
+            continue;
+        cout << i << " ";
+    }
+    return 0;
+}
