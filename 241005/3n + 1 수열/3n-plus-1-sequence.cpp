70fec41e8f44d83c4732001d920f2f77203f0a43
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-10-05 15:16:44 +0900

    Title: 3n + 1 수열, Time: 1074ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241005/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i = 0;
+
+    cin >> n;
+    while(1)
+    {
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = n * 3 + 1;
+        i ++;
+    }
+    cout << i;
+    return 0;
+}
