e7fcb1ae9025888152034c92836abcb002e26940
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-10-06 15:16:02 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i = 0;
+
+    cin >> n;
+    while(1)
+    {
+        if(n / 2 > 0)
+        {
+            n = n / 2;
+            i ++;
+        }
+        else
+            break;
+    }
+    cout << i;
+    return 0;
+}
