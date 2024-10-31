a4709e788e90a7c601ec0c9c2c5634afe7ed86fb
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-10-31 14:24:28 +0900

    Title: 별 그리기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241031/별 그리기/star-drawing.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i = 0;
+    int j;
+
+    cin >> n;
+    while(i < n)
+    {
+        j = 0;
+        while(j < n - i - 1)
+        {
+            cout << " ";
+            j ++;
+        }
+        j = 0;
+        while(j < 2 * i + 1)
+        {
+            cout << "*";
+            j ++;
+        }
+        cout << endl;
+        i ++;
+    }
+    i = 0;
+    while(i < n - 1)
+    {
+        j = 0;
+        while(j < i + 1)
+        {
+            cout << " ";
+            j ++;
+        }
+        j = 0;
+        while(j < n - (2 * i))
+        {
+            cout << "*";
+            j ++;
+        }
+        cout << endl;
+        i ++;
+    }
+    return 0;
+}
