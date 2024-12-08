9e2c1b8984c60419b0a8758368b1bb78be8a86f0
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-12-08 17:26:24 +0900

    Title: 주사위 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241208/주사위 놀이/play-with-dice.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int arr[10];
+
+    for (int i = 0; i < 10; i ++)
+    {
+        cin >> arr[i];
+    }
+	for (int i = 1; i < 7; i++) {
+		int cnt = 0;
+		for (int j = 0; j < 10; j++) {
+			if (arr[j] == i) {
+				cnt++;
+			}
+		}
+		cout << i << " - " << cnt << endl;
+	}
+
+	return 0;
+
+}
+
+
