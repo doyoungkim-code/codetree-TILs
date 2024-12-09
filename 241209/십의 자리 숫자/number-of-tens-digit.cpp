0ffbc5f5f315d7eb18deed394993993e1bb22ea5
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2024-12-09 11:36:52 +0900

    Title: 십의 자리 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/십의 자리 숫자/number-of-tens-digit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n[100];
+    int k[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    for (int i = 0; i < 100; i ++)
+    {
+        cin >> n[i];
+        if (n[i] == 0)
+            break;
+    }
+    for (int i = 0; i < 100; i ++)
+    {
+        if (n[i] == 0)
+            break;
+        if (n[i] / 10 == 0)
+            continue;
+        k[n[i] / 10 - 1] ++;
+    }
+    for (int i = 0; i < 9; i ++)
+    {
+        cout << i + 1 << " - " << k[i] << endl;
+    }
+    return 0;
+}
